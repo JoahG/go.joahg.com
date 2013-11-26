47bd56629c2a6b7fbc8d41b796f22f7b9372b312
Joah Gerstenberg <joahd@gbergfamily.com>
2013-11-26 08:40:54 -0600

    Init commit. Base Rails app with Heroku config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Go::Application.config.secret_token = '6e7e4e7abcbabb4f08641d44f460dc967ddec764d1c12e28319727dadf5e684df5548803299e0917033b9b1b3a82d02e2b8c08d2fb44cec8cf4e27ebea5ea3ee'
